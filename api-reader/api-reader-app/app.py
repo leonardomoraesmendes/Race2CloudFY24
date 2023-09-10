c6533b9e09f2950ca374401ebd8cc0f095b85aa6
whiplash0104 <felipe.basso.vera@gmail.com>
2023-09-10 13:53:12 -0500

    Update app.py

--- api-reader/api-reader-app/app.py
+++ api-reader/api-reader-app/app.py
@@ -1,6 +1,7 @@
 # using flask_restful
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
+from flask_cors import CORS
 from adb import retrieve_data
 import json
 
@@ -8,6 +9,7 @@ import logging
   
 # creating the flask app
 app = Flask(__name__)
+CORS(app)
 # creating an API object
 api = Api(app)
   
